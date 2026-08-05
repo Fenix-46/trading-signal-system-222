4da875c4d6aae515146c52b87c897a9dcbe2f5bf
Fenix-46 <124468747+Fenix-46@users.noreply.github.com>
2026-08-05 16:45:16 +0400

    Fix file path comment in hook-PyQt6.py

--- hooks/hook-PyQt6.py
+++ hooks/hook-PyQt6.py
@@ -1,7 +1,6 @@
-# hook-PyQt6.py
+# hooks/hook-PyQt6.py
 # PyInstaller hook for PyQt6 to ensure Qt plugins and sip are included.
 from PyInstaller.utils.hooks import collect_data_files, collect_submodules
 
-# Collect all PyQt6 submodules (safer) and data files (plugins)
 hiddenimports = collect_submodules('PyQt6')
 datas = collect_data_files('PyQt6')
